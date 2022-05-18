5605bec9e70917b60fc0a3221b41875a35fe85bc
ShantArabaghian <shantarabaghian@outlook.com>
2022-05-18 22:31:25 +0300

    Enter message

---
+++ src/components/CarouselData.js
@@ -0,0 +1,18 @@
+export const SliderData = [
+    {
+      id:1,
+      image:
+        'https://cdn.glitch.global/5288658f-8afb-4ac7-b1fa-300b8de8635f/Upcycled-Jewelry-Projects-to-Make-and-Sell.png?v=1652728642933'
+    },
+    {
+      id:2,
+      image:
+      'https://i.ebayimg.com/thumbs/images/g/g8gAAOSwPjpiF62T/s-l1200.jpg'    
+    },
+    
+    {
+      id:3,
+      image:
+        'https://images.pexels.com/photos/1457801/pexels-photo-1457801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    }
+  ];
