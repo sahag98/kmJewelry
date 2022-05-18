4afadcf6026ba3b58373b131129b6dd4ea265ff1
ShantArabaghian <shantarabaghian@outlook.com>
2022-05-18 22:32:53 +0300

    Enter message

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,45 +1,55 @@
-import React from 'react'
-import "./Carousel.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-bootstrap/Carousel'
-
-const SlideShow = () => {
+import "./Carousel.css";
+import { useState } from "react";
+import Slider from "react-slick";
+import { SliderData } from "./CarouselData";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+function App() {
+  const NextArrow = ({ onClick }) => {
     return (
-      
-      
-      <Carousel> 
-  <Carousel.Item interval={3000}>
-    <img
-      className="d-block"
-      src="https://media.smallbiztrends.com/2022/03/Upcycled-Jewelry-Projects-to-Make-and-Sell.png"
-      alt="First slide"
-    />
-  </Carousel.Item>
-  <Carousel.Item interval={3000}>
-    <img
-      className="d-block"
-      src="https://images.pexels.com/photos/1457801/pexels-photo-1457801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      alt="Second slide"
-    />
-  </Carousel.Item>
-  <Carousel.Item interval={3000}>
-    <img
-      className="d-block"
-      src="https://images.pexels.com/photos/11932917/pexels-photo-11932917.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-      alt="Third slide"
-    />
-  </Carousel.Item>
-  <Carousel.Item interval={3000}>
-    <img
-      className="d-block"
-      src="https://images.pexels.com/photos/2735981/pexels-photo-2735981.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      alt="Third slide"
-    />
-  </Carousel.Item>
-</Carousel>
-       
-       
-    )
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
+      </div>
+    );
+  };
+
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    );
+  };
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    dots: true,
+    fade: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
+
+  return (
+    <div className="brands">
+      <Slider {...settings}>
+        {SliderData.map((img, idx) => (
+          <div className={idx === imageIndex ? "brandslide brandactiveSlide" : "brandslide"}>
+            <img src={img.image} alt="/" />
+            
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
 
-export default SlideShow;
+export default App;
